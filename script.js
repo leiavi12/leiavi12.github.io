86df226176b318c6677c10415d151c464c2ec3a1
leia-vugi <leahmaemeramontetuburan@gmail.com>
2025-12-29 18:35:58 -1200

    my first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const themeToggle = document.getElementById('theme-toggle');
+const themeIcon = document.getElementById('theme-icon');
+const root = document.documentElement;
+
+const savedTheme = localStorage.getItem('data-theme');
+if(savedTheme) {
+    root.setAttribute('data-theme', savedTheme);
+    updateIcon(savedTheme);
+}
+
+if (themeToggle) {
+    themeToggle.addEventListener('click', (e) => {
+        if (e && typeof e.preventDefault === 'function') e.preventDefault();
+
+        const currentTheme = localStorage.getItem('data-theme') || 'light';
+        const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+
+        root.setAttribute('data-theme', newTheme);
+        localStorage.setItem('data-theme', newTheme);
+        updateIcon(newTheme);
+    });
+}
+
+function updateIcon(theme){
+    if(theme === "dark") {
+        themeIcon.classList.remove('sun');
+        themeIcon.classList.add('moon');
+    } else {
+        themeIcon.classList.remove('moon');
+        themeIcon.classList.add('sun');
+    }
+}
